165ae5fbff7a4e285cdb29bad9d10a3d75f0645b
Andreas Pergantis <andreaspergantis@hotmail.com>
2025-09-23 12:36:02 +0200

    Use a dropdown of allowed times instead of time field in forms.py

--- bookings/forms.py
+++ bookings/forms.py
@@ -7,6 +7,24 @@ from .models import Booking
 
 class BookingForm(forms.ModelForm):
     """Form for creating and updating bookings."""
+    time = forms.ChoiceField(choices=[
+        ('14:00', '14.00'),
+        ('14:30', '14.30'),
+        ('15:00', '15.00'),
+        ('15:30', '15.30'),
+        ('16:00', '16.00'),
+        ('16:30', '16.30'),
+        ('17:00', '17.00'),
+        ('17:30', '17.30'),
+        ('18:00', '18.00'),
+        ('18:30', '18.30'),
+        ('19:00', '19.00'),
+        ('19:30', '19.30'),
+        ('20:00', '20.00'),
+        ('20:30', '20.30'),
+        ('21:00', '21.00'),
+        ('21:30', '21.30'),
+    ])
     party_size = forms.ChoiceField(choices=[(i, str(i)) for i in range(1, 9)])
 
     class Meta:
@@ -16,6 +34,4 @@ class BookingForm(forms.ModelForm):
 
         widgets = {
             "date": forms.DateInput(attrs={"type": "date", "class": "form-control"}),
-            "time": forms.TimeInput(attrs={"type": "time", "class": "form-control",
-                                           "step": 1800}),
         }
