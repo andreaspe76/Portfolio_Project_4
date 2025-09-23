f8c9a78214e360efe86412e00206565e05d3c218
Andreas Pergantis <andreaspergantis@hotmail.com>
2025-09-23 11:47:42 +0200

    Create the booking view

--- bookings/views.py
+++ bookings/views.py
@@ -1,7 +1,9 @@
 """Bookings app views
 """
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import BookingForm
 
 
 # Create your views here.
@@ -13,3 +15,18 @@ def home(request):
 def menu(request):
     """Renders the menu page."""
     return render(request, 'menu.html')
+
+
+@login_required
+def book_create(request):
+    """Renders the booking form."""
+    if request.method == "POST":
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            booking = form.save(commit=False)
+            booking.user = request.user
+            booking.save()
+            return redirect("booking_list")
+    else:
+        form = BookingForm()
+    return render(request, "bookings/booking_form.html", {"form": form})
