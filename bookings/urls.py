e8d2b211cb327e907c36861ca16e12262bc77636
Andreas Pergantis <andreaspergantis@hotmail.com>
2025-09-23 11:52:37 +0200

    Update the URL

--- bookings/urls.py
+++ bookings/urls.py
@@ -7,4 +7,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('menu/', views.menu, name='menu'),
+    path('new/', views.booking_create, name='booking_create'),
 ]
